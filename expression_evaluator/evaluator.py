e4001f31ffa68132f8b1abf5dc74f42a6f632c44
Prabin <pravinxtha123@gmail.com>
2026-04-12 14:16:29 +0930

    Implement Tokenizer

---
+++ expression_evaluator/evaluator.py
@@ -0,0 +1,36 @@
+def tokenize(line: str) -> list[str]:
+    # Tokenizes a line of text into a list of tokens
+    tokens = []
+    current_number = ''
+
+    for char in line:
+        # Accumulate digits into current_number
+        if char.isdigit():
+            current_number += char
+        else:
+            # Flush current_number if it exists and is not empty
+            if current_number:
+                tokens.append(f'[NUM:{current_number}]')
+                current_number = ''
+            
+            # Tokenize special characters and operators
+            if char == '(':
+                tokens.append('[LPAREN]')
+            elif char == ')':
+                tokens.append('[RPAREN]')
+            elif char in '+-/*':
+                tokens.append(f'[OP:{char}]')
+            elif char == ' ':
+                # Skip whitespace
+                pass
+            else:
+                # Invalid character encountered - return ERROR
+                return "ERROR"
+    
+    # Flush any remaining number at the end of line
+    if current_number:
+        tokens.append(f'[NUM:{current_number}]')
+    
+    # Add end-of-line marker
+    tokens.append('[END]')
+    return tokens
